1dfb78dc0d806a12eab0369b5e134ab5c4c1bb48
Pierce Thompson <insprill@gmail.com>
2023-07-20 01:36:26 -0400

    Switch back to UMM

--- Main.cs
+++ Main.cs
@@ -1,46 +1,67 @@
 using System;
-using System.Reflection;
-using BepInEx;
-using BepInEx.Configuration;
 using HarmonyLib;
+using UnityModManagerNet;
 
 namespace DvMod.BookletOrganizer
 {
-    [BepInPlugin(GUID, "BookletOrganizer", Version)]
-    public class Main : BaseUnityPlugin
+    [EnableReloading]
+    public static class Main
     {
-        private const string GUID = "com.github.mspielberg.dv-bookletorganizer";
-        private const string Version = "1.0.0";
-        
-        private static Main instance = null!;
-        private Harmony? harmony;
-        private ConfigEntry<bool> enableLogging = null!;
+        public static UnityModManager.ModEntry? mod;
+        public static Settings settings = new Settings();
+        public static bool enabled;
 
-        private void Awake()
+        public static bool Load(UnityModManager.ModEntry modEntry)
         {
-            if (instance != null)
+            mod = modEntry;
+
+            try
             {
-                Logger.LogFatal($"{Info.Metadata.Name} is already loaded!");
-                Destroy(this);
-                return;
+                Settings? loaded = Settings.Load<Settings>(modEntry);
+                if (loaded.version == modEntry.Info.Version)
+                    settings = loaded;
             }
+            catch
+            { }
 
-            instance = this;
+            mod.OnGUI = OnGUI;
+            mod.OnSaveGUI = OnSaveGUI;
+            mod.OnToggle = OnToggle;
 
-            enableLogging = Config.Bind("Debug", "Enable logging", false, "Whether to enable debug logging");
-
-            harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            return true;
         }
 
-        private void OnDestroy()
+        private static void OnGUI(UnityModManager.ModEntry modEntry)
         {
-            harmony?.UnpatchSelf();
+            settings.Draw(modEntry);
+        }
+
+        private static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Harmony harmony = new Harmony(modEntry.Info.Id);
+
+            if (value)
+                harmony.PatchAll();
+            else
+                harmony.UnpatchAll(modEntry.Info.Id);
+            return true;
+        }
+
+        public static void DebugLog(TrainCar car, Func<string> message)
+        {
+            if (car == PlayerManager.Car)
+                DebugLog(message);
         }
 
         public static void DebugLog(Func<string> message)
         {
-            if (instance.enableLogging.Value)
-                instance.Logger.LogDebug(message());
+            if (settings.enableLogging)
+                mod?.Logger.Log(message());
         }
     }
 }
